6e37337f71958658c0f617180fbe6727d6c2a735
Darshan Vaishya <DarshanVaishya3@gmail.com>
2024-11-20 22:14:26 -0500

    Read header

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,7 +8,8 @@ import java.net.Socket;
 
 public class Main {
 	public static void main(String[] args) {
-		// You can use print statements as follows for debugging, they'll be visible when running tests.
+		// You can use print statements as follows for debugging, they'll be visible
+		// when running tests.
 		System.out.println("Logs from your program will appear here!");
 
 		try {
@@ -17,7 +18,7 @@ public class Main {
 			// Allow reuse of address to prevent port in use error
 			serverSocket.setReuseAddress(true);
 			// Wait for server to receive a request
-			Socket clientSocket = serverSocket.accept(); 
+			Socket clientSocket = serverSocket.accept();
 
 			// Get streams and reader from the new connection
 			InputStream input = clientSocket.getInputStream();
@@ -29,15 +30,27 @@ public class Main {
 			System.out.println("Request received: " + line);
 			String[] HttpRequest = line.split(" ");
 
-			if(HttpRequest[1].equals("/")) {
+			if (HttpRequest[1].equals("/")) {
 				output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
 				System.out.println("Accepted new connection");
-			} else if(HttpRequest[1].startsWith("/echo/")) {
+
+			} else if (HttpRequest[1].startsWith("/echo/")) {
 				String path = HttpRequest[1].split("/")[2];
-				String s = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + path.length() + "\r\n\r\n" + path;
+				String s = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + path.length()
+						+ "\r\n\r\n" + path;
 				output.write(s.getBytes());
-			}
-			else {
+
+			} else if (HttpRequest[1].startsWith("/user-agent")) {
+				String userAgent = reader.readLine();
+				while (!userAgent.startsWith("User-Agent"))
+					userAgent = reader.readLine();
+
+				userAgent = userAgent.split(" ")[1];
+				String res = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + userAgent.length()
+						+ "\r\n\r\n" + userAgent;
+				output.write(res.getBytes());
+
+			} else {
 				output.write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
 				System.out.println("Rejected new connection");
 			}
