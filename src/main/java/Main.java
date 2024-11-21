da37c7e3ce40b8af763458286bcba9db4cbd0d2e
Darshan Vaishya <DarshanVaishya3@gmail.com>
2024-11-20 21:23:06 -0500

    Extract URL Path

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,26 +12,35 @@ public class Main {
 		System.out.println("Logs from your program will appear here!");
 
 		try {
+			// Create a server on port 4221
 			ServerSocket serverSocket = new ServerSocket(4221);
+			// Allow reuse of address to prevent port in use error
 			serverSocket.setReuseAddress(true);
-			// Wait for connection from client.
+			// Wait for server to receive a request
 			Socket clientSocket = serverSocket.accept(); 
 
+			// Get streams and reader from the new connection
 			InputStream input = clientSocket.getInputStream();
 			OutputStream output = clientSocket.getOutputStream();
 			BufferedReader reader = new BufferedReader(new InputStreamReader(input));
 
+			// Read request URL
 			String line = reader.readLine();
 			String[] HttpRequest = line.split(" ");
 
+			// If requested URL is the home then accept connection
 			if(HttpRequest[1].equals("/")) {
 				output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-				System.out.println("accepted new connection");
+				System.out.println("Accepted new connection");
 			}
+			// Otherwise reject the connection
 			else {
 				output.write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
+				System.out.println("Rejected new connection");
 			}
 
+			input.close();
+			output.close();
 			serverSocket.close();
 		} catch (IOException e) {
 			System.out.println("IOException: " + e.getMessage());
