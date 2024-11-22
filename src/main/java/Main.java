d63d78cf93799ba92094d9c5551f9691a2c6a471
Darshan Vaishya <DarshanVaishya3@gmail.com>
2024-11-21 21:15:04 -0500

    Fixed formatting

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,23 +4,19 @@ import java.net.Socket;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		// You can use print statements as follows for debugging, they'll be visible
-		// when running tests.
 		System.out.println("Logs from your program will appear here!");
+		// Create a server on port 4221
 		ServerSocket serverSocket = new ServerSocket(4221);
 
 		try {
-			// Create a server on port 4221
 			// Allow reuse of address to prevent port in use error
 			serverSocket.setReuseAddress(true);
-			// Wait for server to receive a request
 
 			while (true) {
 				Socket clientSocket = serverSocket.accept();
 				ConnectionHandler tp = new ConnectionHandler(clientSocket);
 				tp.start();
 			}
-
 		} catch (IOException e) {
 			System.out.println("IOException: " + e.getMessage());
 		} finally {
